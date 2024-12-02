73d3201e8bac1622e3fffa68ea9ae791c2f41d52
Pedro <pedro.mt.morais@ctw.bmwgroup.com>
2024-12-02 18:54:55 +0000

    First exercise advent of code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,55 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+
+
+    /**
+     * first approach
+     */
+//    fun part1(input: List<String>): Int {
+//        val mappedValues = input.map { it.split("   ") }
+//            .map { it[0].toInt() to it[1].toInt() }
+//            .unzip()
+//
+//
+//        val listA = mappedValues.first.sorted()
+//        val listB = mappedValues.second.sorted()
+//
+//        return listA.zip(listB).sumOf { (a, b) -> abs(a - b) }
+//    }
+
+
+    /**
+     * Try hard mode
+     */
+    fun part1(input: List<String>): Int =
+        input
+            .map { ids -> ids.split("   ").let { it[0].toInt() to it[1].toInt() } }
+            .unzip()
+            .let {
+                it.first.sorted() to it.second.sorted()
+            }
+            .let {
+                it.first.zip(it.second).sumOf { (a, b) -> abs(a - b) }
+            }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val mappedValues = input.map { it.split("   ") }
+            .map { it[0].toInt() to it[1].toInt() }
+            .unzip()
+        val list = mappedValues.first.map {
+            val times = mappedValues.second.count { bValue -> bValue == it}
+            times * it
+        }
+        return list.sumOf { it }
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    check(part1(listOf("test_input")) == 1)
+//
+//    // Or read a large test input from the `src/Day01_test.txt` file:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
