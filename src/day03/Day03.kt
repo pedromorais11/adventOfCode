54e434c6e6b0a4e91fa396876cfe3b41d6a5aec1
Pedro <pedro.mt.morais@ctw.bmwgroup.com>
2024-12-10 15:29:41 +0000

    day 3

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -12,17 +12,12 @@ fun main() {
 
     fun part2(input: List<String>): Int =
         input
-            .flatMap { Regex("(.*?)\\b(do(?:n't)?\\b.*?(?=\\bdo(?:n't)?\\b|\$))").findAll(it) }
-            .flatMap { it.groupValues.drop(1) }
-            .filterNot { it.isEmpty() ||  it.startsWith("don't") }
-            .flatMap { Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(it) }
+            .joinToString()
+            .split("do()")
+            .map { it.substringBefore("don't()") }
+            .flatMap {  Regex("mul\\((\\d+),(\\d+)\\)").findAll(it)  }
             .map { it.groupValues.drop(1) }
             .sumOf { it.map(String::toInt).reduce(Int::times) }
-//            .filterNot { it.first().startsWith("don't") }
-//            .map { it.first() }
-//            .flatMap { Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(it) }
-//            .map { it.groupValues.drop(1) }
-//            .sumOf { it.map(String::toInt).reduce(Int::times) }
 
     val input = readInput("day03", "Day03")
     part1(input).println()
---
+++ src/day04/Day04.kt
@@ -0,0 +1,16 @@
+package day04
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int =
+        input.size
+
+    fun part2(input: List<String>): Int =
+        input.size
+
+    val input = readInput("day04", "Day04")
+    part1(input).println()
+    part2(input).println()
+}
